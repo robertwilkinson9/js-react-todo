bb1c38eedd87147c1a3c09c4d95dbea3bff6da18
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-03-08 12:43:28 +0000

    start to add per item buttons - but I need more knowledge so I can pass data up hierarchy - refs?

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,9 +1,54 @@
+import React, { useState } from "react";
+
+const todoArray = [
+	{
+		id: 1,
+		datetime: 202303081030,
+		summary: "learn react",
+		text: "learn how to create react applications",
+	},
+	{
+		id: 2,
+		datetime:202312181000,
+		summary: "prepare for christmas",
+		text: "Put up xmas tree and wait for Santa",
+	},
+];
+
 const TodoItem = ({ todo }) => {
+  const [todos, setTodos] = useState(todoArray); 
+
+  const DeleteTodo = (id) => {
+    setTodos([
+      ...todos,
+    ]);
+  };
+
+  const EditTodo = (id) => {
+    setTodos([
+      ...todos,
+      {
+        id: 3,
+	datetime: 202304081030,
+	summary: "improve typescript",
+	text: "learn how to write better typescript",
+      },
+    ]);
+  };
   return (
     <tr>
       <td>{todo.datetime}</td>
       <td>{todo.summary}</td>
       <td>{todo.text}</td>
+      <td>
+      	<button className="btn btn-primary" onClick={DeleteTodo}>
+       	 Delete
+	</button>
+      </td>
+      <td>
+      	<button className="btn btn-primary" onClick={EditTodo}> Edit
+	</button>
+      </td>
     </tr>
   );
 };
