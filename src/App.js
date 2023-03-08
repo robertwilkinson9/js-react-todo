07edbfc342ad2e2a63531e76ef1d3e88e2c5b32f
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-03-08 10:05:25 +0000

    change to todoList from Todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Banner from './Banner.js';
-import Todolist from './Todolist.js';
+import todoList from './todoList.js';
 import './App.css';
 
 function App() {
   return (
 	  <div>
 	  <Banner />
-	  <Todolist />
+	  <todoList />
 	  </div>
   );
 }
