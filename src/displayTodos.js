6969d51af6260c6ec2f59350688913b541df90b2
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-03-29 12:54:55 +0100

    add logic to sort todo items by datetime cos ...

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -2,10 +2,12 @@ import DisplayTodo from "./displayTodo"
 
 const DisplayTodos = (props) => {
   console.log("DisplayTodos TODO LIST props is ", JSON.stringify(props));
+  const sorted_todos = props.todos.sort((a,b)  => (a.due > b.due) ? 1 : -1);
+  console.log("DisplayTodos TODO sorted_todos is ", JSON.stringify(sorted_todos));
 
   return (
     <>
-         {props.todos?.map((todo) => <DisplayTodo key={todo._id} todos={props.todos} todo={todo} setter={props.setter} edit_id={props.edit_id} seteditmode={props.seteditmode} setid={props.setid} getTodos={props.getTodos} /> )}
+         {sorted_todos?.map((todo) => <DisplayTodo key={todo._id} todos={props.todos} todo={todo} setter={props.setter} edit_id={props.edit_id} seteditmode={props.seteditmode} setid={props.setid} getTodos={props.getTodos} /> )}
     </>
   );
 };
